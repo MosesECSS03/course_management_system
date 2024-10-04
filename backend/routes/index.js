ab38ead47539c46abc3fea386e124d427d6b27a1
MosesECSS03 <moses_lee@ecss.org.sg>
2024-10-04 12:44:09 +0800

    Moses testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Connected to Node JS!!!!");
+  res.send("Connected to Node JS!!!! Bye bye");
   // res.render('index', { title: 'Express' });
 });
 
