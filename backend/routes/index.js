dc7439487e418570d3b857069aa1194c90b9f571
MosesECSS03 <moses_lee@ecss.org.sg>
2024-10-04 13:52:44 +0800

    Revert to Original

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Connected to Node JS!!!! Bye bye");
-  // res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
