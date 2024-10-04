cc15d5a23c12ea4b19b013ad666cd1e0bcd837d3
MosesECSS03 <moses_lee@ecss.org.sg>
2024-10-04 12:32:45 +0800

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Connected to Node JS");
+  res.send("Connected to Node JS!!!!");
   // res.render('index', { title: 'Express' });
 });
 
