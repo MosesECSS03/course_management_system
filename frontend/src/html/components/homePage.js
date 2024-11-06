cf82d14ddd308b23585c10165d7c804672b06fdc
MosesECSS03 <moses_lee@ecss.org.sg>
2024-11-06 17:12:35 +0800

    Update homePage.js

--- frontend/src/html/components/homePage.js
+++ frontend/src/html/components/homePage.js
@@ -270,7 +270,7 @@
 
     componentDidMount() {
       // Start the inactivity detection timeout
-      sessionStorage.clear();
+      //sessionStorage.clear();
       this.resetInactivity();
       // Adding event listeners to reset inactivity
       window.addEventListener('mousemove', this.resetInactivity);
