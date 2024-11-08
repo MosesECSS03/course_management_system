c175fe6dd73b88699d78946775fd08b193b53eb9
MosesECSS03 <moses_lee@ecss.org.sg>
2024-11-08 10:26:39 +0800

    Update accountsSection.js

--- frontend/src/html/components/sub/accountsSection.js
+++ frontend/src/html/components/sub/accountsSection.js
@@ -24,8 +24,8 @@ class AccountsSection extends Component {
   {
     try {
       this.setState({ loading: true });
-      //var response = await axios.post(`https://moses-ecss-backend.azurewebsites.net/accountDetails`, { "purpose": "retrieve"});
-      var response = await axios.post(`http://localhost:3001/accountDetails`, { "purpose": "retrieve"});
+      var response = await axios.post(`https://moses-ecss-backend.azurewebsites.net/accountDetails`, { "purpose": "retrieve"});
+      //var response = await axios.post(`http://localhost:3001/accountDetails`, { "purpose": "retrieve"});
       console.log(response.data.result);
       var roles = this.getAllRoles(response.data.result);
 
