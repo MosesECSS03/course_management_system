751da98e765ba923fcc85e4ad166ebd31a34dd05
MosesECSS03 <moses_lee@ecss.org.sg>
2024-11-14 17:25:07 +0800

    Update submitDetails.js

--- frontend/src/html/components/sub/submitDetails.js
+++ frontend/src/html/components/sub/submitDetails.js
@@ -8,9 +8,12 @@ class SubmitDetailsSection extends React.Component {
         <div className="input-group1">
           <span className="submit-detail-text">
             Thank you for registration! It is currently under review, and we will contact you shortly.
-            </span>
-            </div>
+          </span>
+          <span className="submit-detail-text">
+            感谢您的注册！我们正在审核中，会尽快与您联系。
+          </span>
         </div>
+      </div>
     );
   }
 }
