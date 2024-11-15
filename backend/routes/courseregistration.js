5e6787aa7736056804cba525c8b3356394fbdce3
MosesECSS03 <moses_lee@ecss.org.sg>
2024-11-15 17:05:03 +0800

    Update courseregistration.js

--- backend/routes/courseregistration.js
+++ backend/routes/courseregistration.js
@@ -12,7 +12,7 @@ function getCurrentDateTime() {
     const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
     const year = now.getFullYear();
 
-    const hours = String(now.getHours()).padStart(2, '0'); // 24-hour format
+    const hours = String(now.getHours()+8).padStart(2, '0'); // 24-hour format
     const minutes = String(now.getMinutes()).padStart(2, '0'); // Ensure two digits
     const seconds = String(now.getSeconds()).padStart(2, '0'); // Ensure two digits
 
