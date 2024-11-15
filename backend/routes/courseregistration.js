8f69833fb02a8d0970be643968732118e82eae5d
MosesECSS03 <moses_lee@ecss.org.sg>
2024-11-15 23:38:22 +0800

    Update courseregistration.js

--- backend/routes/courseregistration.js
+++ backend/routes/courseregistration.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var RegistrationController = require('../Controller/Registration/RegistrationController');
-var RecieptController = require('../Controller/Registration/RecieptController');
-var ReceiptGenerator = require('../Controller/Receipt/ReceiptController');
+var ReceiptController = require('../Controller/Receipt/ReceiptController');
 var PdfGenerator = require('../Others/Pdf/PdfGenerator');
 
 function getCurrentDateTime() {
@@ -91,7 +90,7 @@ router.post('/', async function(req, res, next)
         var date = currentDateTime.date;
         var time = currentDateTime.time;
         var result = await controller.updateRemarks(req.body.id, req.body.remarks, req.body.staff, date, time);
-        var controller1 = new RecieptController();
+        var controller1 = new ReceiptController();
         var result1 = await controller1.deleteReceipt(req.body.id);
         return res.json({"result": result1}); 
     }
