22945c53dcfd73e7f0126c9be030dc747e720edb
MosesECSS03 <moses_lee@ecss.org.sg>
2024-12-05 09:47:34 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add any other methods you want to support
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add any other methods you want to support ok
   allowedHeaders: ['Content-Type', 'Authorization', 'Content-Disposition'],
   exposedHeaders: ['Content-Disposition'], // Add this line to expose the header
 }));
