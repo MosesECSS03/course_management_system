f24056a138590775dd1862461b398a0bdb3e5594
MosesECSS03 <moses_lee@ecss.org.sg>
2024-12-09 16:24:55 +0800

    Update createAccountsSection.js

--- frontend/src/html/components/sub/createAccountsSection.js
+++ frontend/src/html/components/sub/createAccountsSection.js
@@ -152,9 +152,9 @@ class CreateAccountsSection extends Component {
 
   isValidEmail = (email) => {
     // Regular expression for validating email format
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$/;
     return emailPattern.test(email);
-}
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
