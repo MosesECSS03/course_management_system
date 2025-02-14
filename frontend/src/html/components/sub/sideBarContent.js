e134ec7f5486682fa3aebc69edba9dbfc8dbce7e
MosesECSS03 <moses_lee@ecss.org.sg>
2025-02-14 09:15:45 +0800

    Change To Localhost

--- frontend/src/html/components/sub/sideBarContent.js
+++ frontend/src/html/components/sub/sideBarContent.js
@@ -57,11 +57,11 @@ class SideBarContent extends Component {
 
     getAccessRight = async (accountId) => {
         try {
-          const response = await axios.post('https://moses-ecss-backend.azurewebsites.net/accessRights', {
-          // await axios.post('http://localhost:3001/accessRights', {
-                "purpose": "retrieveAccessRight",
-                "accountId": accountId
-             });
+          const response = //await axios.post('https://moses-ecss-backend.azurewebsites.net/accessRights', {
+                           await axios.post('http://localhost:3001/accessRights', {
+                                "purpose": "retrieveAccessRight",
+                                "accountId": accountId
+                            });
             console.log(response);
 
             // Store the access rights in state
