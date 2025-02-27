900962ad168833ab674e3179215c549a4cac459c
MosesECSS03 <moses_lee@ecss.org.sg>
2025-02-27 21:03:38 +0800

    Remove Singpass

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,6 @@ var accountDetailsRouter = require("./routes/accountDetails");
 var accessRightsRouter = require("./routes/accessRights");
 var receiptRouter = require("./routes/receipt");
 var invoiceRouter = require("./routes/invoice");
-var singpassRouter = require("./routes/singpass");
-
-
 
 var app = express();
 // view engine setup
@@ -42,7 +39,6 @@ app.use("/accountDetails", accountDetailsRouter);
 app.use("/accessRights", accessRightsRouter);
 app.use("/receipt", receiptRouter);
 app.use("/invoice", invoiceRouter);
-app.use("/singpass", singpassRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
